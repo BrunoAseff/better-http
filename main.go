f707e58c81f03a35ac57f804cc0acf9d6a86531b
Bruno Aseff <brunoaseff2@gmail.com>
2026-01-01 20:53:36 -0300

    refactor: update main function

--- main.go
+++ main.go
@@ -1,40 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func getGreetingForHour(hour int) string {
-	var greeting string
-
-	switch {
-	case hour >= 0 && hour < 6:
-		greeting = "Good night!"
-	case hour >= 6 && hour < 12:
-		greeting = "Good morning!"
-	case hour >= 12 && hour < 18:
-		greeting = "Good afternoon!"
-	case hour >= 18 && hour < 24:
-		greeting = "Good evening!"
-	default:
-		greeting = "Hello!"
-	}
-
-	return greeting
-}
-
-func getGreeting() string {
-	return getGreetingForHour(time.Now().Hour())
-}
+import "fmt"
 
 func main() {
 
-	projectName := "Better HTTP"
-
-	greeting := getGreeting()
-
-	log := fmt.Sprintf("%s Running %s...", greeting, projectName)
-
-	fmt.Println(log)
+	fmt.Println("I hope I get the job!")
 }
