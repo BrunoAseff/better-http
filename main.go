36a99ec79df0f5187f69740a1bf3ee89b93cc477
Bruno Aseff <brunoaseff2@gmail.com>
2026-01-01 23:01:43 -0300

    feat: create logic to read the file

--- main.go
+++ main.go
@@ -1,8 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+
+	if err != nil {
+		fmt.Println("File could not be opened")
+	}
+
+	defer file.Close()
+
+	buf := make([]byte, 1024)
+	var num int
+
+	for {
+		n, err := file.Read(buf)
+
+		num += n
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println("Error reading the file")
+			break
+		}
+	}
+
+	if len(buf) > 0 {
+
+		for i := 0; len(buf) > i; i += 8 {
+
+			if i > num {
+				break
+			}
+
+			lo := i
+			hi := i + 8
+
+			fmt.Printf("read: %v\n", string(buf[lo:hi]))
+
+		}
+	}
 }
