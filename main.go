d3ebd3c5f4c932a3021661e5722a47068e76bf06
Bruno Aseff <brunoaseff2@gmail.com>
2026-01-02 09:21:56 -0300

    refactor: update the code to utilize goroutines and channels

--- main.go
+++ main.go
@@ -6,6 +6,52 @@ import (
 	"os"
 )
 
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	buf, err := io.ReadAll(file)
+	ch := make(chan string)
+
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil
+	}
+
+	if len(buf) > 0 {
+
+		bytesToAdd := []byte{}
+
+		go func() {
+			for i := 0; i < len(buf); i += 8 {
+				lo := i
+				hi := i + 8
+
+				if hi > len(buf) {
+					hi = len(buf)
+				}
+
+				chars := buf[lo:hi]
+
+				for _, char := range chars {
+
+					if string(char) == "\n" {
+						ch <- string(bytesToAdd)
+
+						bytesToAdd = []byte{}
+
+						continue
+					}
+
+					bytesToAdd = append(bytesToAdd, char)
+				}
+
+			}
+
+			defer close(ch)
+		}()
+	}
+
+	return ch
+}
+
 func main() {
 
 	file, err := os.Open("messages.txt")
@@ -15,42 +61,12 @@ func main() {
 		return
 	}
 
+	ch := getLinesChannel(file)
+
+	for str := range ch {
+		fmt.Printf("read: %v\n", str)
+	}
+
 	defer file.Close()
 
-	buf, err := io.ReadAll(file)
-
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-
-	if len(buf) > 0 {
-
-		bytesToAdd := []byte{}
-
-		for i := 0; i < len(buf); i += 8 {
-			lo := i
-			hi := i + 8
-
-			if hi > len(buf) {
-				hi = len(buf)
-			}
-
-			chars := buf[lo:hi]
-
-			for _, char := range chars {
-
-				if string(char) == "\n" {
-					fmt.Printf("read: %v\n", string(bytesToAdd))
-
-					bytesToAdd = []byte{}
-
-					continue
-				}
-
-				bytesToAdd = append(bytesToAdd, char)
-			}
-
-		}
-	}
 }
