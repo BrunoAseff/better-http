678aa16bebd8df029301cacd2b64c4cb1dbe5e5e
Bruno Aseff <brunoaseff2@gmail.com>
2026-01-03 16:29:32 -0300

    feat: create udp example

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	const ADDRESS = ":42069"
+	const NETWORK = "udp"
+
+	add, err := net.ResolveUDPAddr(NETWORK, ADDRESS)
+
+	if err != nil {
+		fmt.Println("Could resolve UDP Addr", err)
+	}
+
+	conn, err := net.DialUDP(NETWORK, nil, add)
+
+	if err != nil {
+		fmt.Println("Could not create connection", err)
+	}
+
+	defer conn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf(">")
+
+		str, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Printf("An error occurred while reading the string", err)
+		}
+
+		{
+			_, err := conn.Write([]byte(str))
+
+			if err != nil {
+				fmt.Printf("An error occurred while writing in the connection", err)
+			}
+		}
+
+	}
+}
