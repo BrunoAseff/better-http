2c737f8717a187e1d65fa1e1c169a64bd0c02982
Bruno Aseff <brunoaseff2@gmail.com>
2026-01-07 21:44:43 -0300

    feat: add bytesNum in parseRequestLine return

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -27,9 +28,7 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		return nil, err
 	}
 
-	str := strings.Split(string(data), "\r\n")
-
-	requestLine, err := parseRequestLine(str[0])
+	requestLine, _, err := parseRequestLine(data)
 
 	if err != nil {
 		return nil, err
@@ -41,14 +40,29 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 
 }
 
-func parseRequestLine(line string) (RequestLine, int, error) {
+func parseRequestLine(request []byte) (rl RequestLine, bytesNum int, err error) {
 
-	sections := strings.Split(line, " ")
+	separator := []byte{'\r', '\n'}
+
+	lines := bytes.Split(request, separator)
+
+	if len(lines) < 2 {
+
+		err := errors.New("The separator was not found")
+
+		return RequestLine{}, 0, err
+	}
+
+	requestLine := lines[0]
+
+	bytesNum = len(requestLine) + len(separator)
+
+	sections := strings.Split(string(requestLine), " ")
 
 	if len(sections) != 3 {
 		err := errors.New("the request line is in the incorrect format")
 
-		return RequestLine{}, err
+		return RequestLine{}, 0, err
 	}
 
 	httpSections := strings.Split(sections[2], "/")
@@ -56,7 +70,7 @@ func parseRequestLine(line string) (RequestLine, int, error) {
 	if len(httpSections) != 2 {
 		err := errors.New("invalid HTTP version")
 
-		return RequestLine{}, err
+		return RequestLine{}, 0, err
 	}
 
 	method := sections[0]
@@ -66,12 +80,12 @@ func parseRequestLine(line string) (RequestLine, int, error) {
 	if method == "" {
 		err := errors.New("method was not provided in the request line")
 
-		return RequestLine{}, err
+		return RequestLine{}, 0, err
 	}
 
 	for _, c := range method {
 		if c < 'A' || c > 'Z' {
-			return RequestLine{}, fmt.Errorf("invalid method: %s", method)
+			return RequestLine{}, 0, fmt.Errorf("invalid method: %s", method)
 		}
 	}
 
@@ -79,12 +93,12 @@ func parseRequestLine(line string) (RequestLine, int, error) {
 
 		err := fmt.Errorf("invalid HTTP version\nExpected: HTTP/1.1\nReceived: %v", httpVersion)
 
-		return RequestLine{}, err
+		return RequestLine{}, 0, err
 	}
 
 	return RequestLine{
 		Method:        method,
 		HttpVersion:   httpVersion,
 		RequestTarget: requestTarget,
-	}, nil
+	}, bytesNum, nil
 }
