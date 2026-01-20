6962993210c2c4a7c90e70c88b761c4fbd5096c0
Bruno Aseff <brunoaseff2@gmail.com>
2026-01-20 17:24:44 -0300

    feat: add loop to parse function

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -14,40 +14,45 @@ func NewHeaders() Headers {
 }
 
 func (h Headers) Parse(data []byte) (int, bool, error) {
+	totalRead := 0
 
-	idx := bytes.Index(data, []byte("\r\n"))
+	for {
+		currentData := data[totalRead:]
+		idx := bytes.Index(currentData, []byte("\r\n"))
 
-	if idx == -1 {
-		return 0, false, nil
+		if idx == -1 {
+			return totalRead, false, nil
+		}
+
+		if idx == 0 {
+			return totalRead + 2, true, nil
+		}
+
+		line := currentData[:idx]
+		colonIdx := bytes.IndexByte(line, ':')
+
+		if colonIdx == -1 {
+			return 0, false, errors.New("malformed header: no colon found")
+		}
+
+		keyBytes := line[:colonIdx]
+		valBytes := line[colonIdx+1:]
+
+		if len(keyBytes) > 0 && keyBytes[len(keyBytes)-1] == ' ' {
+			return 0, false, errors.New("header field name cannot have trailing whitespace before colon")
+		}
+
+		key := string(bytes.TrimSpace(keyBytes))
+		val := string(bytes.TrimSpace(valBytes))
+
+		if !isAllowed(key) {
+			return 0, false, errors.New("malformed header: invalid characters in key")
+		}
+
+		h[key] = val
+
+		totalRead += idx + 2
 	}
-
-	if idx == 0 {
-		return 2, true, nil
-	}
-
-	line := data[:idx]
-
-	colonIdx := bytes.IndexByte(line, ':')
-
-	if colonIdx == -1 {
-		return 0, false, errors.New("malformed header: no colon found")
-	}
-
-	keyBytes := line[:colonIdx]
-	valBytes := line[colonIdx+1:]
-
-	if len(keyBytes) > 0 && keyBytes[len(keyBytes)-1] == ' ' {
-		return 0, false, errors.New("header field name cannot have trailing whitespace before colon")
-	}
-
-	key := string(bytes.TrimSpace(keyBytes))
-	val := string(bytes.TrimSpace(valBytes))
-
-	h[key] = val
-
-	n := idx + 2
-
-	return n, false, nil
 }
 
 func isAllowed(s string) bool {
